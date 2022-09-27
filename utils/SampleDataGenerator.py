00c812147c14479ea889070e518cffd334a93575
Cenker <34512627+purelog1c@users.noreply.github.com>
2022-09-27 17:23:11 +0300

    FileName and TimeStamp Generation implemented (Testing required)

--- utils/SampleDataGenerator.py
+++ utils/SampleDataGenerator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datetime
-import openpyxl
+# import openpyxl
 import random
 import os
 
@@ -11,33 +11,44 @@ class SampleDataGenerator():
         start = datetime.datetime.strptime(startDate, "%d-%m-%Y")
         end = datetime.datetime.strptime(endDate, "%d-%m-%Y")
         date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+        print(date_generated)
         return date_generated
+    
 
     def generateTimeStamp(self, dataLength):
         startTime = random.randrange(1, 24)
         endTime = random.randrange(1, 24)
 
         if(startTime == endTime):
-            self.generateTimeStamp()
+            self.generateTimeStamp(dataLength=dataLength)
         if(startTime > endTime):
             tempTime = startTime
             startTime = endTime
             endTime = tempTime
+        
+        
 
         dataSet = pd.date_range(str(startTime)+":00:00", str(endTime)+":00:00", freq="1s").strftime('%H:%M:%S')
-        dataFrame = pd.DataFrame(dataSet[:dataLength],columns=["TIME"])   
+        dataFrame = pd.DataFrame([dataSet[:dataLength]],columns=["TIME"])   
         
         return dataFrame
 
-    def generateXMLOrCSVFile(self, startDate, endDate, dataLength):
+    
+
+    def generateXMLOrCSVFile(self, startDate, endDate, dataLength,xmlOrCSV):
         localDirectory = os.getcwd()+"\\Samples"
         fileNames = SampleDataGenerator.generateDate(startDate,endDate)
         for fileName in fileNames:
+            fileName = fileName.strftime("%d-%m-%Y")
+            print(fileName)
             if not(os.path.exists(localDirectory + "\\" + fileName)):
                 generatedDataSet = self.generateTimeStamp(dataLength)
-
+                if(str(xmlOrCSV).lower() == "csv"):
+                    generatedDataSet.to_csv(localDirectory + '\\' + fileName +'.csv',encoding='utf-8', index=False)
+                if(str(xmlOrCSV).lower() == "xlsx"):
+                    generatedDataSet.to_excel(localDirectory + '\\' + fileName +'.xlsx')
 
                 
 a = SampleDataGenerator()
-b = a.generateTimeStamp(10)
-print(b)
+b = a.generateXMLOrCSVFile("27-09-2022","30-09-2022",10000, "csv")
+# print(b)
